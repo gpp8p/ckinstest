d0edd30d9d3631128368f68da925ce24462ca095
George Pipkin <gpp8p@Virginia.EDU>
2020-06-29 15:09:46 -0400

    new user

--- src/components/newUserEntry.vue
+++ src/components/newUserEntry.vue
@@ -2,9 +2,9 @@
     <div v-if="adminUserSelect==this.NEW_USER">
         <span class="newUserBody">
             <span class="inputPrompt">User Name:</span><span><input ref="name" type="text" size="40" v-model="name" @keydown.tab.exact = "nameCheck" required = true /></span>
-            <span class="inputPrompt">User Email:</span><span><input ref="email" type="text" size="50" v-model="name" @keydown.tab.exact = "emailCheck" required = true /></span>
-            <span class="inputPrompt">User Password:</span><span><input ref="email" type="text" size="40" v-model="name" @keydown.tab.exact = "pwCheck" required = true /></span>
-            <span class="inputPrompt">Password Repeat:</span><span><input ref="email" type="text" size="40" v-model="name" @keydown.tab.exact = "pwrCheck" required = true /></span>
+            <span class="inputPrompt">User Email:</span><span><input ref="email" type="text" size="50" v-model="email" @keydown.tab.exact = "emailCheck" required = true /></span>
+            <span class="inputPrompt">User Password:</span><span><input ref="pw1" type="text" size="40" v-model="password" @keydown.tab.exact = "pwCheck" required = true /></span>
+            <span class="inputPrompt">Password Repeat:</span><span><input ref="pw2" type="text" size="40" v-model="password2" @keydown.tab.exact = "pwrCheck" required = true /></span>
         </span>
         <span class="newUserFooter">
             <a href="#" class="linkStyle" v-on:click="newUserSave" >Create This User</a>
@@ -13,20 +13,45 @@
 </template>
 
 <script>
+    import axios from "axios";
+
     export default {
         name: "newUserEntry",
         props:{
             adminUserSelect:{
                 type:Number,
                 required:true
+            },
+            orgId:{
+                type:Number,
+                required: true
             }
         },
         data(){
             return{
                 NEW_USER:1,
-                SELECT_USER:0
+                SELECT_USER:0,
+                name: '',
+                email: '',
+                password: '',
+                password2: ''
             }
         },
+        methods: {
+            newUserSave(){
+                axios.post('http://localhost:8000/api/shan/createUser?XDEBUG_SESSION_START=19311', {
+
+                }).then(response=>
+                {
+//            debugger;
+                    console.log(response);
+                    this.$emit('userSaved');
+//                this.$refs.editGrid.createBlankLayout(msg[2],msg[3],msg[1],msg[0]);
+                }).catch(function(error) {
+                    console.log(error);
+                });
+            }
+        }
     }
 </script>
 
